a2520476480c9fadb9c33aec619a0a4b8c6952a4
Ezhkin-Kot <ezkot.tokze@gmail.com>
2026-02-17 16:08:01 +0400

    wip

--- TyumentsevGraphics/build/Libraries/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ezhkinkot/Projects/csit/computer-graphics/TyumentsevGraphics/Libraries/raylib
-# Build directory: /Users/ezhkinkot/Projects/csit/computer-graphics/TyumentsevGraphics/build/Libraries/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
