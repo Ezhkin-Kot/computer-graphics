f387ab6b96681917c945aed375f11be0254abfe8
Ezhkin-Kot <ezkot.tokze@gmail.com>
2026-05-11 12:57:15 +0400

    feat: modify transform.hpp

--- Projects/lab6/transform.hpp
+++ Projects/lab6/transform.hpp
@@ -10,10 +10,26 @@ inline Mat3 translate(float Tx, float Ty) {
     return res;
 }
 
+inline Mat4 translate(float Tx, float Ty, float Tz) {
+    Mat4 res = Mat4(1.f);
+    res[0][3] = Tx;
+    res[1][3] = Ty;
+    res[2][3] = Tz;
+    return res;
+}
+
 inline Mat3 scale(float Sx, float Sy) {
     Mat3 res = Mat3(1.f);
     res[0][0] = Sx;
     res[1][1] = Sy;
+    return res;
+}
+
+inline Mat4 scale(float Sx, float Sy, float Sz) {
+    Mat4 res = Mat4(1.f);
+    res[0][0] = Sx;
+    res[1][1] = Sy;
+    res[2][2] = Sz;
     return res;
 }
 
