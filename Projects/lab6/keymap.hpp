53c80d70eb23eadcff5858b3f20387b952edb2d5
Ezhkin-Kot <ezkot.tokze@gmail.com>
2026-05-15 00:56:59 +0400

    feat: new keymaps

--- Projects/lab6/keymap.hpp
+++ Projects/lab6/keymap.hpp
@@ -2,6 +2,8 @@
 
 #include "matrix.hpp"
 #include "screen.hpp"
+#include "transform.hpp"
+#include <algorithm>
 #include <raylib.h>
 
 inline void keymap_handler(Screen &s) {
@@ -87,5 +89,92 @@ inline void keymap_handler(Screen &s) {
         } else {
             s.l += 1;
         }
+    }
+    if (IsKeyDown(KEY_Y)) {
+        s.T = rotate(0.1f, Vec3(0, 0, 1)) * s.T;
+    }
+
+    if (IsKeyDown(KEY_G)) {
+        if (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT)) {
+            s.T = rotateP(0.1f, Vec3(1, 0, 0), s.P) * s.T;
+        } else {
+            s.T = rotate(0.1f, Vec3(1, 0, 0)) * s.T;
+        }
+    }
+    if (IsKeyDown(KEY_F)) {
+        if (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT)) {
+            s.T = rotateP(-0.1f, Vec3(0, 1, 0), s.P) * s.T;
+        } else {
+            s.T = rotate(-0.1f, Vec3(0, 1, 0)) * s.T;
+        }
+    }
+    if (IsKeyDown(KEY_H)) {
+        if (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT)) {
+            s.T = rotateP(0.1f, Vec3(0, 1, 0), s.P) * s.T;
+        } else {
+            s.T = rotate(0.1f, Vec3(0, 1, 0)) * s.T;
+        }
+    }
+
+    if (IsKeyDown(KEY_K)) {
+        if (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT)) {
+            s.b -= 1.0f;
+        } else {
+            s.b += 1.0f;
+        }
+    }
+    if (IsKeyDown(KEY_L)) {
+        if (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT)) {
+            s.r -= 1.0f;
+        } else {
+            s.r += 1.0f;
+        }
+    }
+    if (IsKeyDown(KEY_U)) {
+        if (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT)) {
+            s.n -= 0.2f;
+        } else {
+            s.n += 0.2f;
+        }
+        s.n = std::max(0.1f, s.n);
+        s.n = std::min(s.n, s.f - 0.1f);
+    }
+    if (IsKeyDown(KEY_O)) {
+        if (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT)) {
+            s.f -= 0.2f;
+        } else {
+            s.f += 0.2f;
+        }
+        s.f = std::max(s.n + 0.1f, s.f);
+    }
+    if (IsKeyDown(KEY_B)) {
+        Vec3 view_dir = norm(s.P - s.S);
+        float step = 0.2f;
+        if (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT)) {
+            if (length(s.P - s.S) - step >= 0.1f) {
+                s.S += view_dir * step;
+                s.initWorkPars();
+            }
+        } else {
+            s.S -= view_dir * step;
+            s.initWorkPars();
+        }
+    }
+    if (IsKeyDown(KEY_Z)) {
+        if (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT)) {
+            s.fovy_work -= 0.1f;
+        } else {
+            s.fovy_work += 0.1f;
+        }
+        s.fovy_work = std::max(0.3f, s.fovy_work);
+        s.fovy_work = std::min(3.0f, s.fovy_work);
+    }
+    if (IsKeyDown(KEY_X)) {
+        if (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT)) {
+            s.aspect_work -= 0.05f;
+        } else {
+            s.aspect_work += 0.05f;
+        }
+        s.aspect_work = std::max(0.01f, s.aspect_work);
     }
 }
